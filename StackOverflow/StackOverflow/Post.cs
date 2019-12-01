cf3e39a3bc796fca2154a40933d294a57a32fc12
Irek Romaniuk <irekromaniuk@ireks-air.lan>
2019-12-01 18:35:49 -0500

    StackOverflow added

---
+++ StackOverflow/StackOverflow/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace StackOverflow
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public int Votes { get; private set; }
+        public Post()
+        {
+            Created = DateTime.Now;
+        }
+        public int UpVote()
+        {
+            return Votes++;
+        }
+        public int DownVote()
+        {
+            return Votes--;
+        }
+    }
+}
---
+++ StackOverflow/StackOverflow/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StackOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var post = new Post();
+            post.Title = "The Title";
+            Console.WriteLine(post.Title);
+            Console.WriteLine(post.Votes);
+            Console.WriteLine(post.Created);
+            post.UpVote();
+            post.UpVote();
+            post.DownVote();
+            post.UpVote();
+            Console.WriteLine(post.Votes);
+
+        }
+    }
+}
