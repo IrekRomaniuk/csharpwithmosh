cb02cd11cdaae9d2757c042bf25a6966211948df
Irek Romaniuk <irekromaniuk@ireks-air.lan>
2019-12-01 15:48:12 -0500

    initial

---
+++ Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace ProjetStopwatch
+{
+    
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(default(DateTime));
+            // Start, Stop and Duration
+            Console.WriteLine("Start, Stop and Duration");
+            var stopWatch = new Stopwatch.Stopwatch1();
+            stopWatch.Start();
+            Thread.Sleep(1000);
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.Duration());
+
+            //Stop Twice
+            Console.WriteLine("Stop twice");
+            stopWatch.Stop();
+            stopWatch.Stop();
+
+            //Start Twice
+            Console.WriteLine("Start twice");
+            try
+            {
+                stopWatch.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Started twice {0}",ex);
+            }
+            
+
+
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch/Stopwatch1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Stopwatch
+{
+    public class Stopwatch1
+    {
+        private DateTime _start;
+        private DateTime _stop;
+        /*public Stopwatch1()
+        {
+            this._time = DateTime.Now;
+        }*/
+        public void Start()
+        {
+            
+            if (this._start < this._stop)
+            {
+                throw new InvalidOperationException("Invalid...");
+            }
+            this._start = DateTime.Now;
+
+        }
+        public void Stop()
+        {
+            
+            if (this._start < this._stop)
+            {
+                throw new InvalidOperationException("Invalid...");
+            }
+            this._stop = DateTime.Now;
+        }
+        public TimeSpan Duration()
+        {
+            return _stop - this._start;
+        }
+    }
+}
