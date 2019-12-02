1a60a89e75f05fd8741d1d32c00fcdf638f91183
Irek Romaniuk <irekromaniuk@Ireks-MacBook-Air.local>
2019-12-02 10:10:51 -0500

    Stack added

---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private List<object> _stack = new List<object>();
+        public void Push(object obj)
+        {
+            if (_stack == null)
+            {
+                throw new InvalidOperationException("Invalid ...");
+            }
+            _stack.Add(obj);
+            //_stack.Reverse();
+            /*foreach (var item in _stack)
+            {
+                Console.WriteLine("Item: {0}",item);
+            }*/
+
+        }
+        public object Pop()
+        {
+            var result = _stack[_stack.Count - 1];
+            //Console.WriteLine("Result: {0}", result);
+            _stack.RemoveAt(_stack.Count - 1);
+            return result;
+        }
+        public void Clear()
+        {
+            _stack.RemoveRange(0, _stack.Count - 1);
+        }
+
+    }
+}
